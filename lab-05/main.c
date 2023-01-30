1820cd40aa81aa0b4d56453be94e37c147b04602
korn <kornkubzaza@gmail.com>
2023-01-30 18:21:49 +0700

    Update

--- lab-05/main.c
+++ lab-05/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-	int i = 1;
-	while (i > 0) {
-		i = i + 1;
-		if (i < 0) {
-			printf("i was %10d before\n", i-1);
+	int i = -1;
+	while (i < 0) {
+		i = i - 1;
+		if (i > 0) {
+			printf("i was %10d before\n", i+1);
 			printf("i  is %10d now\n", i);
 			break;
 		}
